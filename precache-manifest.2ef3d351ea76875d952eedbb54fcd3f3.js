2875b4a820abdbba93b9d8b4687374ae543d20e4
jamilyam <jamilyam312@gmail.com>
2020-09-17 22:44:53 +0600

    Updates

---
+++ precache-manifest.2ef3d351ea76875d952eedbb54fcd3f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c5149c244d2e272db3cdff900b9ecb",
+    "url": "/contact__app/index.html"
+  },
+  {
+    "revision": "25c273dd44ae10bf0f80",
+    "url": "/contact__app/static/css/main.14dbf378.chunk.css"
+  },
+  {
+    "revision": "796a99951433d3ab949f",
+    "url": "/contact__app/static/js/2.7fbb4499.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contact__app/static/js/2.7fbb4499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25c273dd44ae10bf0f80",
+    "url": "/contact__app/static/js/main.a67704f1.chunk.js"
+  },
+  {
+    "revision": "f8e7a4489381e790e46a",
+    "url": "/contact__app/static/js/runtime-main.56e9b18d.js"
+  }
+]);
